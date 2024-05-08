1fd7aa1351426afb5c054285015e28f612143277
wes1909 <wesleipereira721@gmail.com>
2024-05-08 18:16:25 -0300

    Construtores criados

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,9 +1,23 @@
 public class Veiculo {
 
+//Atibutos
     int ano;
     String modelo;
     String cor;
     float km;
 
+// Construtores
+public Veiculo(){
+
+}
+
+public Veiculo(int ano, String modelo, String cor, float km) {
+
+    this.ano = ano;
+    this.modelo = modelo;
+    this.cor = cor;
+    this.km = km;
+
+}
 
 }
